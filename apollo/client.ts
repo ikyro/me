409b4d9f84be07ca018f0594c06a5ab3cec18d5a
ikyro <ikyroch@gmail.com>
2022-11-13 15:15:43 -0300

    fix(apollo): fields merge

--- apollo/client.ts
+++ apollo/client.ts
@@ -2,7 +2,18 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    // thanks github copilot
+    typePolicies: {
+      Query: {
+        fields: {
+          viewer: {
+            merge: false,
+          },
+        },
+      },
+    },
+  }),
   headers: {
     authorization: `bearer ${process.env.GITHUB_TOKEN}`,
   },

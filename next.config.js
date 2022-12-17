c2fe6a3ac7323cbc7e92d14a0ca6ce69574465fa
ikyro <ikyroch@gmail.com>
2022-12-17 15:19:44 -0300

    remove experimental app

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  experimental: {
-    appDir: true,
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  images: {
+    /* add image from github user domain */
+    domains: ['avatars.githubusercontent.com'],
   },
 }
 

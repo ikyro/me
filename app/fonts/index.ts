e5563eb0e4644bd212b8d11c1c373485e753a163
ikyro <ikyroch@gmail.com>
2022-12-17 15:27:44 -0300

    unuse app

--- app/fonts/index.ts
+++
@@ -1,6 +0,0 @@
-import localFont from '@next/font/local'
-
-export const pixelFont = localFont({
-  src: './04b03b.ttf',
-  variable: '--pixel-font',
-})
--- app/random/page.tsx
+++
@@ -1,9 +0,0 @@
-const Random = () => {
-  return (
-    <div>
-      <h1>I want to suicide</h1>
-    </div>
-  )
-}
-
-export default Random
